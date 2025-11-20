cc3818e062f70990929680b404e5c554cb3a74a3
voidexiled <l20070518@cdmadero.tecnm.mx>
2025-11-20 05:27:39 -0600

    nerf_buff(FocusRegenerationLow): 5 -> 3

--- Content/Buffs/BuffFocusRegenerationLow.cs
+++ Content/Buffs/BuffFocusRegenerationLow.cs
@@ -4,21 +4,20 @@ using Terraria.ModLoader;
 
 namespace MagicianClass.Content.Buffs;
 
-public class BuffFocusRegenerationLow: ModBuff
+public class BuffFocusRegenerationLow : ModBuff
 {
-    public const int FocusRegenAmountExtra = 1;
+    public const int FocusRegenAmountExtra = 3;
     public override LocalizedText Description => base.Description.WithFormatArgs(FocusRegenAmountExtra);
+
     public override void SetStaticDefaults()
     {
         Main.debuff[Type] = false;
         Main.buffNoSave[Type] = true;
         Main.pvpBuff[Type] = true;
-        
     }
 
     public override void Update(Player player, ref int buffIndex)
     {
-           player.GetModPlayer<GlobalPlayer>().FocusResourceRegenAmount += 5;
-           
+        player.GetModPlayer<GlobalPlayer>().FocusResourceRegenAmount += FocusRegenAmountExtra;
     }
 }
